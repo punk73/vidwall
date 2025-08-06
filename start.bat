04864d60060902a6426b5af4487c16f1ce421e12
Punk73 <teguhsetiawan171@gmail.com>
2025-08-06 10:40:42 +0700

    set the port

--- start.bat
+++ start.bat
@@ -3,16 +3,16 @@ title Video Wall Launcher
 cd /d %~dp0
 
 REM -- Start Node.js proxy server (in a new terminal)
-start cmd /k "node server.js"
+start cmd /k "node server.js --port 3000"
 
 REM -- Wait a bit for proxy to start
 timeout /t 3 /nobreak > nul
 
 REM -- Start static server with npx serve (in another terminal)
-start cmd /k "npx serve"
+start cmd /k "npx serve -p 3001"
 
 REM -- Wait for static server to start
 timeout /t 3 /nobreak > nul
 
 REM -- Launch Chrome in fullscreen (kiosk) mode
-start chrome --kiosk http://localhost:3000/index.html
+start chrome --kiosk http://localhost:3001/index.html
